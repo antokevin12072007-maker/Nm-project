845f4c9334705f2434e174906fd93dfefe350782
antokevin12072007-maker <antokevin12072007@gmail.com>
2025-09-10 14:55:39 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("theme-toggle");
-const body = document.body;
-
-toggle.addEventListener("change", () => {
-  body.classList.toggle("dark", toggle.checked);
-});
